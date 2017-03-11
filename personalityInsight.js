8d62632112cbe9810af54ec3b0afe6e895a7e143
Alfred <alfred.hong@outlook.com>
2017-03-11 11:02:46 -0800

    created personalityInsightFile

---
+++ personalityInsight.js
@@ -0,0 +1,25 @@
+var PersonalityInsightsV3 = require('watson-developer-cloud/personality-insights/v3');
+var personality_insights = new PersonalityInsightsV3({
+  username: "dc9370f1-4bb2-400c-b971-e8002a334efe",
+  password: "kgZ3FDvpsSmH",
+  version_date: '2016-10-20'
+});
+
+var params = {
+  // Get the content items from the JSON file.
+  content_items: require('./profile.json').contentItems,
+  consumption_preferences: true,
+  raw_scores: true,
+  headers: {
+    'accept-language': 'en',
+    'accept': 'application/json'
+  }
+};
+
+personality_insights.profile(params, function(error, response) {
+  if (error)
+    console.log('Error:', error);
+  else
+    console.log(JSON.stringify(response, null, 2));
+  }
+);
