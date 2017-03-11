ce965b75fad9b67fd79a742d30aaa1e74067d850
IisLaw <iris.yt.law@gmail.com>
2017-03-11 12:19:14 -0800

    testing sample text

--- personalityInsight.js
+++ personalityInsight.js
@@ -5,9 +5,26 @@ var personality_insights = new PersonalityInsightsV3({
   version_date: '2016-10-20'
 });
 
+var textForTesting = "Call me Ishmael. Some years ago-never mind how long precisely-having "
+    + "little or no money in my purse, and nothing particular to interest me on shore, "
+    + "I thought I would sail about a little and see the watery part of the world. "
+    + "It is a way I have of driving off the spleen and regulating the circulation. "
+    + "Whenever I find myself growing grim about the mouth; whenever it is a damp, "
+    + "drizzly November in my soul; whenever I find myself involuntarily pausing before "
+    + "coffin warehouses, and bringing up the rear of every funeral I meet; and especially "
+    + "whenever my hypos get such an upper hand of me, that it requires a strong moral "
+    + "principle to prevent me from deliberately stepping into the street, and methodically "
+    + "knocking people's hats off-then, I account it high time to get to sea as soon as I can. "
+    + "This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself "
+    + "upon his sword; I quietly take to the ship. There is nothing surprising in this. "
+    + "If they but knew it, almost all men in their degree, some time or other, cherish "
+    + "very nearly the same feelings towards the ocean with me. There now is your insular "
+    + "city of the Manhattoes, belted round by wharves as Indian isles by coral reefs-commerce surrounds "
+    + "it with her surf. Right and left, the streets take you waterward."
+
 var params = {
   // Get the content items from text field
-  string text = "",
+  text: textForTesting,
   consumption_preferences: true,
   raw_scores: true,
   headers: {
@@ -20,6 +37,21 @@ personality_insights.profile(params, function(error, response) {
   if (error)
     console.log('Error:', error);
   else
-    console.log(JSON.stringify(response, null, 2));
+    var unique = {};
+    res = JSON.stringify(response, null, 2);
+    //console.log(Object.keys(response));
+    //console.log(res);
+    var personality = response["personality"];
+    for (var x in personality){
+        //console.log(personality[x]);
+      if (personality[x]["name"] == "Openness"){
+          unique["Openness"] = Number(personality[x]["percentile"]).toFixed(3) * 100;
+      }
+        if (personality[x]["name"] == "Openness"){
+            unique["Openness"] = Number(personality[x]["percentile"]).toFixed(3) * 100;
+        }
+    }
+    console.log(unique);
+
   }
 );
